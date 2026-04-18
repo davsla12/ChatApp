f145d608c82eaf23b6158df4930b011e0a0170d4
davsla12 <122469382+davsla12@users.noreply.github.com>
2026-04-18 10:38:46 +0200

    added screen_delete and Doc

--- screen.h
+++ screen.h
@@ -10,23 +10,38 @@ struct Scane{
     WINDOW* window;
     int rows,collums; //define max size of the window
     int pos_y,pos_x;
-    int timer; //how often call main
-    std::list<sprite*> sprites;
+    int timer; //how often call main TODO
+    std::list<Sprite*> sprites;
     void (*input)(int key); //waits for input from user
     int (*main)();
     void (*render)(); //rendering somethink outside sprites
-    int ID = 0;
+    int ID = -1;
 };
 
 extern int screen_y;
 extern int screen_x;
 
+//gives size of scane
+//id 0 is main screen
 int GetSizey(int id);
 int GetSizex(int id);
+
+//add`s scane into scane`s stack
+//TODO: RmScane
+//\returns scane ID
 int AddScane(Scane* add);
-int init_screen();
+
+//initialized ncurses and sets COLOR_PAIR`s
+int screen_init();
+
+//ends ncurses
+void screen_delete();
+
+//Create ncurses window
 void CreateWin(Scane* scane,int rows,int collums,int y,int x);
+
+//Functions to call Scanes function
 int SInput();
 int SMain();
-int update_scr();
+int screen_update();
 #endif
